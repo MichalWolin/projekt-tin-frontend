12d974d1f907db74bfdda6b91909f61c915fc712
Michał Woliński <s29239@pjwstk.edu.pl>
2025-01-13 14:28:33 +0100

    validate AddTournament

--- src/AddTournament.js
+++ src/AddTournament.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 function AddTournament() {
   const [cookies] = useCookies(['user']);
@@ -9,6 +10,17 @@ function AddTournament() {
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [gender, setGender] = useState('male');
+  const navigate = useNavigate();
+  const [nameError, setNameError] = useState('');
+  const [startDateError, setStartDateError] = useState('');
+  const [endDateError, setEndDateError] = useState('');
+  const [genderError, setGenderError] = useState('');
+
+  useEffect(() => {
+    if (!cookies.user || cookies.user.role !== "manager") {
+      navigate("/");
+    }
+  }, [cookies.user]);
 
   const handleNameChange = (event) => {
     setName(event.target.value);
@@ -36,17 +48,74 @@ function AddTournament() {
   };
 
   const handleSubmit = (event) => {
-    //TODO: Walidacja :)
+    event.preventDefault();
     setErrorMessage('');
     setSuccessMessage('');
-    event.preventDefault();
+    setNameError('');
+    setStartDateError('');
+    setEndDateError('');
+    setGenderError('');
+    let error = false;
+
+    if (name.length < 3) {
+      setNameError("Nazwa turnieju musi zawierać co najmniej 3 znaki.");
+      error = true;
+    }
+
+    if (name.length > 255) {
+      setNameError("Nazwa turnieju nie może zawierać więcej niż 255 znaków.");
+      error = true;
+    }
+
+    if (!startDate) {
+      setStartDateError("Data rozpoczęcia turnieju jest wymagana.");
+      error = true;
+    }
+
+    if (!endDate) {
+      setEndDateError("Data zakończenia turnieju jest wymagana.");
+      error = true;
+    }
+
+    const startDateObj = new Date(startDate);
+    const endDateObj = new Date(endDate);
+
+    if (startDateObj < new Date()) {
+      setStartDateError("Data rozpoczęcia turnieju nie może być w przeszłości.");
+      error = true;
+    }
+
+    if (startDateObj > endDateObj) {
+      setEndDateError("Data zakończenia turnieju nie może być wcześniejsza niż data rozpoczęcia.");
+      error = true;
+    }
+
+    if (endDateObj - startDateObj < 24 * 60 * 60 * 1000) {
+      setEndDateError("Turniej musi trwać co najmniej 1 dzień.");
+      error = true;
+    }
+
+    if (endDateObj - startDateObj > 24 * 60 * 60 * 1000 * 14) {
+      setEndDateError("Turniej nie może trwać dłużej niż 14 dni.");
+      error = true;
+    }
+
+    if (gender !== "male" && gender !== "female") {
+      setGenderError("Nieprawidłowa płeć.");
+      error = true;
+    }
+
+    if (error) {
+      return;
+    }
+
     fetch("http://localhost:3000/tournaments", {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        "name": name,
+        "tournament_name": name,
         "start_date": startDate,
         "end_date": endDate,
         "manager_id": cookies.user.id,
@@ -56,6 +125,13 @@ function AddTournament() {
     .then(response => {
       if (response.status === 201) {
         setSuccessMessage("Pomyślnie dodano turniej.");
+        setName('');
+        setStartDate('');
+        setEndDate('');
+        setGender('male');
+        setTimeout(() => {
+          setSuccessMessage('');
+        }, 5000);
       } else {
         setErrorMessage("Wystąpił nieoczekiwany błąd.");
       }
@@ -72,15 +148,19 @@ function AddTournament() {
       <form>
         <label>Nazwa turnieju</label>
         <input type="text" placeholder="Nazwa turnieju" value={name} onChange={handleNameChange} />
+        <p className="error-message">{nameError}</p>
         <label>Data rozpoczęcia</label>
         <input type="date" value={formatDate(startDate)} onChange={handleStartDateChange} />
+        <p className="error-message">{startDateError}</p>
         <label>Data zakończenia</label>
         <input type="date" value={formatDate(endDate)} onChange={handleEndDateChange} />
+        <p className="error-message">{endDateError}</p>
         <label>Płeć</label>
         <select name="gender" onChange={handleGenderChange}>
           <option value="male">Mężczyzna</option>
           <option value="female">Kobieta</option>
         </select>
+        <p className="error-message">{genderError}</p>
         <button className="form-button" onClick={handleSubmit}>Dodaj turniej</button>
         <p className="error-message">{errorMessage}</p>
         <p className="success-message">{successMessage}</p>
